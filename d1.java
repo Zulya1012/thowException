04371f070547a43d42e4f29a0e8a18ce3cf9c4ce
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-29 21:31:24 +0400

    Add files via upload

---
+++ d1.java
@@ -0,0 +1,10 @@
+public class d1{
+  public static void main(String[] arg){
+    int a=2;
+    int b=0;
+    if (b==0)throw new ArithmeticException("Divide by 0 cannot!");
+    int c=a/b;
+    System.out.println(c);
+  }
+ }
+ 
