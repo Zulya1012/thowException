326f749e5fca97d9906e78302ebb4f02b27bab1c
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-29 21:31:55 +0400

    Update d1.java

--- d1.java
+++ d1.java
@@ -1,10 +1,9 @@
-public class d1{
-  public static void main(String[] arg){
-    int a=2;
-    int b=0;
-    if (b==0)throw new ArithmeticException("Divide by 0 cannot!");
-    int c=a/b;
+public class d1 {
+  public static void main(String[] arg) {
+    int a = 2;
+    int b = 0;
+    if (b == 0) throw new ArithmeticException("Divide by 0 cannot!");
+    int c = a / b;
     System.out.println(c);
   }
- }
- 
+}
